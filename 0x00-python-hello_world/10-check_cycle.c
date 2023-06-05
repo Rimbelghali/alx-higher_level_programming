6702cbcb99a7cb68161c4d93f2efbf77ddeeffe9
Rimbelghali <rime.belghali@gmail.com>
2023-06-05 15:32:10 -0700

    10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,6 @@
+#include "lists.h"
+/**
+* check_cycle - function checks if a singly linked list has a cycle in it.
+* @list: pointer to the beginning of the node
+* Return: 0 if no cycle, 1 if there is a cycle
+*/
