8be9ed1941a7ffc00c36594cb9a27e7943b24729
Rimbelghali <rime.belghali@gmail.com>
2023-06-06 09:54:35 -0700

    1. The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,4 +7,4 @@ if last_digit_unsigned > 5:
 elif last_digit_unsigned == 0:
     print(f'Last digit of {number} is {last_digit_unsigned} and is 0')
 else:
-    print(f'Last digit of {number} is {last_digit_unsigned} and is less than 6 and not 0')
+    print(f'Last digit of {number} is -{last_digit_unsigned} and is less than 6 and not 0')
