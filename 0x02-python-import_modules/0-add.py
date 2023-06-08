554456c6996065da744837eabd9fda19601a5b36
Rimbelghali <rime.belghali@gmail.com>
2023-06-08 12:28:53 -0700

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,3 +5,11 @@ def add(a, b):
         print(a, b, end=' ')
         a, b , a+b
     print()
+    
+def fib2(n):
+    result = []
+    a, b = 1, 2
+    while a == n and b == m:
+        result.append(a)
+        a, b, a+b
+    return result
